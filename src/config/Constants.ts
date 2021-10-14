0c7be5e8385450593f3be7689642c44b90234e37
brunood07 <brunood07@gmail.com>
2021-10-14 12:04:29 -0300

    projeto finalizado

---
+++ src/config/Constants.ts
@@ -0,0 +1,4 @@
+export const config = {
+    API_URL: "http://localhost:5000",
+    MONGO_CONNECTION: 'mongodb+srv://brunood07:bruno123@urlshortener.wjmll.mongodb.net/urlShortener?retryWrites=true&w=majority'
+}
